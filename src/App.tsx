2029fe5d278e8cb7360319eb755dd1fd6a925ea0
desssty <dimonmikryukov@gmail.com>
2025-06-19 23:29:40 +0400

    feat: add lazy loading pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import HeroDetailPage from "./pages/HeroDetailPage";
-import SearchPage from "./pages/SearchPage";
 import Layout from "./components/common/Layout";
+import { Suspense, lazy } from "react";
 
-function App() {
+const HomePage = lazy(() => import("./pages/HomePage"));
+const HeroDetailPage = lazy(() => import("./pages/HeroDetailPage"));
+const SearchPage = lazy(() => import("./pages/SearchPage"));
+
+export default function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/hero/:id" element={<HeroDetailPage />} />
-        <Route path="/search" element={<SearchPage />} />
-      </Route>
-    </Routes>
+    <Suspense
+      fallback={<div className="text-center mt-10">Загрузка страницы...</div>}
+    >
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/hero/:id" element={<HeroDetailPage />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
-
-export default App;
