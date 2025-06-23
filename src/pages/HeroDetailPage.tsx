a5fcfb295c42399e16d5d2659dce1e94901ece8d
desty <dimonmikryukov@gmail.com>
2025-06-24 02:03:19 +0400

    fix: add basic design for HeroDetailsPage

--- src/pages/HeroDetailPage.tsx
+++ src/pages/HeroDetailPage.tsx
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import { useMembers } from "../context/MembersContext";
+import PanelButton from "../components/common/PanelButton";
+import medalIcon from "../assets/medalIcon.svg";
+import unknownPerson from "../assets/unknownPerson.jpg";
 
 export default function HeroDetailPage() {
   const { state, dispatch } = useMembers();
@@ -21,13 +24,115 @@ export default function HeroDetailPage() {
       console.log("Дальше данных нет");
     }
   };
+
+  const btnSize = { width: "428px", height: "69px" };
   return (
     <>
       <Helmet>
         <title>Герой</title>
       </Helmet>
-      <h1>{member.name}</h1>
-      <button onClick={handleNext}>Следующий</button>
+      <div className="w-full px-[5rem]">
+        <div className="flex flex-row border-t border-t-[#8B8785] pt-[46px] justify-between gap-0">
+          <div className="flex flex-col gap-[111px]">
+            <div className="flex flex-col gap-[2.5rem]">
+              <h1 className="text-5xl text-[#2B2A29] font-bold">
+                {member.name || "?"}
+              </h1>
+              <div className="flex flex-col text-[#792426] ">
+                <div className="flex flex-row gap-[72px]">
+                  <div>
+                    <p className="text-[2rem]  italic font-semibold">
+                      Год рождения
+                    </p>
+                    <p className="text-[#2B2A29] text-[1.5rem]">
+                      {member.yearStartAt || "?"}
+                    </p>
+                  </div>
+                  <div>
+                    <p className="text-[2rem]  italic font-semibold">
+                      Место рождения
+                    </p>
+                    <p className="text-[#2B2A29] text-[1.5rem]">
+                      {member.city || "?"}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-row gap-[72px]">
+                  <div>
+                    <p className="text-[2rem]  italic font-semibold">Звание</p>
+                    <p className="text-[#2B2A29] text-[1.5rem]">
+                      {member.ranks || "?"}
+                    </p>
+                  </div>
+                  <div>
+                    <p className="text-[2rem]  italic font-semibold">
+                      Призван в армию
+                    </p>
+                    <p className="text-[#2B2A29] text-[1.5rem]">
+                      {member.calledUponDate || "?"}
+                    </p>
+                  </div>
+                  <div>
+                    <p className="text-[2rem] italic font-semibold">
+                      Как погиб
+                    </p>
+                    <p className="text-[#2B2A29] text-[1.5rem]">
+                      {member.howDie || "?"}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-row gap-[72px]">
+                  <div>
+                    <p className="text-[2rem]  italic font-semibold">
+                      Место гибели (захоронение)
+                    </p>
+                    <p className="text-[#2B2A29] text-[1.5rem]">
+                      {member.placeDeath || "?"}
+                    </p>
+                  </div>
+                  <div>
+                    <p className="text-[2rem]  *:italic font-semibold">
+                      Дата гибели
+                    </p>
+                    <p className="text-[#2B2A29] text-[1.5rem]">
+                      {member.monthDeath || "?"}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-[1rem]">
+              <div className="flex flex-row gap-[1rem]">
+                <PanelButton
+                  type="link"
+                  href="/"
+                  label="НА ГЛАВНУЮ"
+                  active={false}
+                  {...btnSize}
+                />
+                <PanelButton
+                  type="button"
+                  icon={medalIcon}
+                  label="CЛЕДУЮЩИЙ ГЕРОЙ"
+                  active={true}
+                  {...btnSize}
+                />
+              </div>
+              <p className="text-[#2B2A29]">
+                Для стены памяти информация получена от родных, близких и друзей
+                героев
+              </p>
+            </div>
+          </div>
+          <div className="">
+            <img
+              src={member.image || unknownPerson}
+              alt={member.name}
+              className="w-[428px] h-[571px] object-cover"
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
