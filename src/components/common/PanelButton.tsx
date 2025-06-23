0dfd02948d33b107b394f2e458437a204791e084
desty <dimonmikryukov@gmail.com>
2025-06-23 19:26:12 +0400

    fix: add some buttons for russian keyboard

--- src/components/common/PanelButton.tsx
+++ src/components/common/PanelButton.tsx
@@ -11,6 +11,7 @@ interface PanelButtonProps {
   bordered?: boolean;
   width?: string;
   height?: string;
+  transparentWhite?: boolean;
 }
 
 export default function PanelButton({
@@ -23,16 +24,38 @@ export default function PanelButton({
   bordered = true,
   width = "280px",
   height = "69px",
+  transparentWhite = false,
 }: PanelButtonProps) {
-  const baseClasses = `flex justify-center items-center text-[1.125rem] no-underline`;
+  const baseClasses = `flex justify-center items-center text-[1.125rem] text-light no-underline select-none transition-colors duration-300 ease-in-out`;
   const activeClasses = "bg-[#CF3337] text-white";
+
+  const transparentWhiteClasses =
+    "bg-transparent border-2 border-white text-white";
+
+  const transparentGrayClasses =
+    "bg-transparent border-2 border-[#514F4D] text-[#514F4D]";
+
   const inactiveClasses = bordered
-    ? "bg-transparent border-2 border-[#514F4D] text-[#514F4D]"
+    ? transparentWhite
+      ? transparentWhiteClasses
+      : transparentGrayClasses
+    : transparentWhite
+    ? "bg-transparent border border-white text-white"
     : "bg-transparent border border-[#514F4D] text-[#514F4D]";
+
+  const hoverActiveClasses = active
+    ? "hover:bg-[#b82f32] active:bg-[#a1282a]"
+    : bordered
+    ? transparentWhite
+      ? "hover:text-[#CF3337] hover:border-[#CF3337] active:text-[#9e292b] active:border-[#9e292b]"
+      : "hover:text-[#CF3337] hover:border-[#CF3337] active:text-[#9e292b] active:border-[#9e292b]"
+    : "hover:bg-[#514F4D] hover:text-white active:bg-[#423f3c]";
+
+  const cursorClass = type === "button" ? "cursor-pointer" : "";
 
   const classNames = `${baseClasses} ${
     active ? activeClasses : inactiveClasses
-  }`;
+  } ${hoverActiveClasses} ${cursorClass}`;
 
   const content = (
     <div className="flex flex-row gap-[15.5px] justify-center items-center">
@@ -41,10 +64,7 @@ export default function PanelButton({
     </div>
   );
 
-  const style: React.CSSProperties = {
-    width,
-    height,
-  };
+  const style: React.CSSProperties = { width, height };
 
   if (type === "link") {
     return (
@@ -55,7 +75,12 @@ export default function PanelButton({
   }
 
   return (
-    <button className={classNames} onClick={onClick} style={style}>
+    <button
+      className={classNames}
+      onClick={onClick}
+      style={style}
+      type="button"
+    >
       {content}
     </button>
   );
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { Helmet } from "react-helmet";
 import VirtualKeyboard from "../components/common/VirtualKeyboard";
+import PanelButton from "../components/common/PanelButton";
 
 function isDesktop() {
   if (typeof navigator === "undefined") return true;
   const userAgent = navigator.userAgent;
+  console.log("UserAgent:", navigator.userAgent);
+
   return !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     userAgent
   );
@@ -13,6 +16,7 @@ function isDesktop() {
 export default function SearchPage() {
   const [value, setValue] = useState("");
   const [showKeyboard, setShowKeyboard] = useState(false);
+  const btnSize = { width: "576px", height: "69px" };
 
   const handleFocus = () => {
     if (isDesktop()) {
@@ -29,25 +33,41 @@ export default function SearchPage() {
       <Helmet>
         <title>Поиск</title>
       </Helmet>
-      <div>SEARCH</div>
-      <div>
-        <input
-          value={value}
-          className="mb-4"
-          onFocus={handleFocus}
-          onChange={handleChange}
-        />
-        {showKeyboard && (
-          <VirtualKeyboard
-            onKeyPress={(key) => setValue((prev) => prev + key)}
-            onBackspace={() => setValue((prev) => prev.slice(0, -1))}
-            onEnter={() => console.log("Submit")}
-            onNext={() => console.log("Next field")}
-            onPrev={() => console.log("Prev field")}
-            onClose={() => setShowKeyboard(false)}
+
+      <div className="search-container p-4 max-w-[800px] mx-auto">
+        {/* Контейнер для заголовка и инпута */}
+        <div className="header-input-container mb-6">
+          <h1 className="text-white text-[2.5rem] text-light mb-4">
+            ПОИСК ПО БАЗЕ ГЕРОЕВ
+          </h1>
+          <input
+            placeholder="Кого вы ищите?"
+            value={value}
+            className="w-[45vw] h-[5rem] text-[2rem] text-white italic p-3 rounded border border-gray-400 bg-transparent placeholder-white"
+            onFocus={handleFocus}
+            onChange={handleChange}
           />
-        )}
+        </div>
+
+        <PanelButton
+          type="link"
+          href="/"
+          label="НА ГЛАВНУЮ"
+          active={false}
+          transparentWhite={true}
+          {...btnSize}
+        />
       </div>
+      {showKeyboard && (
+        <VirtualKeyboard
+          onKeyPress={(key) => setValue((prev) => prev + key)}
+          onBackspace={() => setValue((prev) => prev.slice(0, -1))}
+          onEnter={() => console.log("Submit")}
+          onNext={() => console.log("Next field")}
+          onPrev={() => console.log("Prev field")}
+          onClose={() => setShowKeyboard(false)}
+        />
+      )}
     </>
   );
 }
